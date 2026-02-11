698d333be00ac5dc073cead140983d909303641c
Avijit Rana <avijitranar@spotify.com>
2026-02-11 11:29:28 +0530

    Added Code Base Initialization

---
+++ src/main/java/com/java/mercatox/MercatoxApplication.java
@@ -0,0 +1,13 @@
+package com.java.mercatox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercatoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercatoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/mercatox/MercatoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.mercatox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercatoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
